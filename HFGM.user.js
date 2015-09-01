91ea889c668602f36e228d76883778ffc9d294a8
MeshCollider <dobsonsa68@gmail.com>
2015-09-02 11:17:15 +1200

    Create HFGM.user.js

---
+++ HFGM.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         My Fancy New Userscript
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        http://www.techradar.com/news/internet/the-beginner-s-guide-to-greasemonkey-scripting-598247
+// @grant        none
+// ==/UserScript==
